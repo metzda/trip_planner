4e2b3e50ca294950f17982526bc7b6f2b40b2b36
Dan Metz <daniel.metz@gmail.com>
2016-03-12 11:30:10 -0500

    end of static ui demo

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var router = require('express').Router();
 var Promise = require('bluebird');
 var models = require('../models');
+var swig = require('swig');
 
 var Hotel = models.Hotel,
     Restaurant = models.Restaurant,
@@ -19,4 +20,18 @@ router.get('/', function(req, res, next) {
             res.render('index', {hotels: data[0], restaurants: data[1], activities: data[2]});
         })
         .catch(next);
+});
+
+router.post('/hotel', function(req, res, next) {
+    Hotel.findById(req.body.hotel)
+    .then(function(hotel) {
+        // add hotel name to list snippet at bottom for proper day
+        console.log(swig.renderFile('../views/trip_list_item.html', {item_name: hotel.name}));
+        //$('#added-hotels').append()
+        // add map tag based on hotel place lat/long
+    })
+    .catch(next);
+    
+    
+    res.redirect('/');
 });
